78b4016c9ee0a2caefd3a76a25204c784cb46efa
Anton Golosnichenko <golosnichenko@artics.ru>
2021-04-17 01:02:59 +0300

    0.12 Работа с REST API через GraphQL - часть 1

--- graphql/db.js
+++ graphql/db.js
@@ -1,54 +1,8 @@
-let movies = [
-  {
-    id: 1,
-    name: "Властелин Колец",
-    score: 8,
-  },
-  {
-    id: 2,
-    name: "Один Дома",
-    score: 7,
-  },
-  {
-    id: 3,
-    name: "Джентельмены",
-    score: 6,
-  },
-  {
-    id: 4,
-    name: "Звездные войны",
-    score: 9,
-  },
-  {
-    id: 5,
-    name: "Бабушка легкого поведения",
-    score: 1,
-  },
-];
+import fetch from "node-fetch";
 
-export const getMovies = () => movies;
+const API_URL = "https://yts.mx/api/v2/list_movies.json";
 
-export const getById = (id) => {
-  const filteredMovies = movies.find((movie) => id === movie.id);
-  return filteredMovies;
-};
-
-export const deleteMovie = (id) => {
-  const cleanedMovies = movies.filter((movie) => movie.id !== id);
-  if (movies.length > cleanedMovies.length) {
-    movies = cleanedMovies;
-    return true;
-  } else {
-    return false;
-  }
-};
-
-export const addMovie = (name, score) => {
-  const newMovie = {
-    id: movies.length + 1,
-    name,
-    score,
-  };
-  movies.push(newMovie);
-  return newMovie;
-};
+export const getMovies = (limit, rating) =>
+  fetch(`${API_URL}`)
+    .then((res) => res.json())
+    .then((json) => json.data.movies);
