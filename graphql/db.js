1b08294db1e044d76999d0f44bed0a8e57073d76
MikhailBobrov <mikhailurban13@yandex.ru>
2022-08-18 11:41:41 +0500

    done

--- graphql/db.js
+++ graphql/db.js
@@ -1,6 +1,7 @@
 import axios from "axios";
+import fetch from "node-fetch";
 const BASE_URL = "https://yts.mx/api/v2/";
-const LIST_MOVIES_URL = `${BASE_URL}list_movies.json`;
+let LIST_MOVIES_URL = `${BASE_URL}list_movies.json`;
 const MOVIE_DETAILS_URL = `${BASE_URL}movie_details.json`;
 const MOVIE_SUGGESTIONS_URL = `${BASE_URL}movie_suggestions.json`;
 
@@ -42,4 +43,62 @@ export const getSuggestions = async (id) => {
     },
   });
   return movies;
+};
+
+let moviesTest = [
+  {
+    id: 1,
+    name: "Властенлин колец",
+    score: 8,
+  },
+  {
+    id: 2,
+    name: "Теория большного взрыва",
+    score: 9,
+  },
+  {
+    id: 3,
+    name: "Во все тяжкие",
+    score: 9,
+  },
+];
+
+export const getMovies2 = (limit, rating) => {
+  if (limit > 0) {
+    LIST_MOVIES_URL += `limit=${limit}`;
+  }
+  if (rating > 0) {
+    LIST_MOVIES_URL += `&minimum_raiting=${rating}`;
+  }
+  return fetch(LIST_MOVIES_URL)
+    .then((res) => res.json())
+    .then((json) => json.data.movies);
+};
+
+export const getMoviesTest = () => moviesTest;
+
+export const getByMoviesId = (id) => {
+  const filteredMovies = moviesTest.find((movie) => id === movie.id);
+  return filteredMovies;
+};
+
+// мутации
+export const deleteMovie = (id) => {
+  const cleanedMovies = moviesTest.filter((movie) => movie.id !== id);
+  if (moviesTest.length > cleanedMovies.length) {
+    moviesTest = cleanedMovies;
+    return true;
+  } else {
+    return false;
+  }
+};
+
+export const addMovie = (name, score) => {
+  const newMovie = {
+    id: moviesTest.length + 1,
+    name,
+    score,
+  };
+  moviesTest.push(newMovie);
+  return newMovie;
 };
--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,10 +1,29 @@
-import { getMovies, getMovie, getSuggestions } from "./db";
+import {
+  getMovies,
+  getMovie,
+  getSuggestions,
+  getByMoviesId,
+  deleteMovie,
+  addMovie,
+  getMoviesTest,
+  getMovies2,
+} from "./db";
 
 const resolvers = {
   Query: {
     movies: (_, { limit, rating }) => getMovies(limit, rating),
     movie: (_, { id }) => getMovie(id),
     suggestions: (_, { id }) => getSuggestions(id),
+
+    // не использую
+    movies2: (_, { limit, rating }) => getMovies2(limit, rating),
+    moviesTest: () => getMoviesTest(),
+    movieTest: (_, { id }) => getByMoviesId(id),
+  },
+
+  Mutation: {
+    addMovie: (_, { name, score }) => addMovie(name, score),
+    deleteMovie: (_, { id }) => deleteMovie(id),
   },
 };
 
